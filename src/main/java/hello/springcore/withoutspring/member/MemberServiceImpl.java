1a2a4106d0ad96d734ae06429b50b71a88b5990e
logan-mac <ralla0405@gmail.com>
2025-04-21 17:42:10 +0900

    docs: 코드의 문제점 설명에 DIP와 OCP원칙을 위반하고 있다는 내용 추가

--- src/main/java/hello/springcore/withoutspring/member/MemberServiceImpl.java
+++ src/main/java/hello/springcore/withoutspring/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package hello.springcore.withoutspring.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    // 분명히 추상화(인터페이스)를 의존하는것처럼 보이지만 구현체도 같이 의존하고 있다. DIP을 위반하고 있는거다.
+    // 분명히 추상화(인터페이스)를 의존하는것처럼 보이지만 구현체도 같이 의존하고 있다. DIP와 OCP를 위반하고 있는거다.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
